d97261f563f92c8a81f98326c456b6f0285133f6
heeyoung <heeyoung12360@gmail.com>
2015-01-19 18:20:25 +0900

    first commit

---
+++ app/src/main/java/app/com/example/heeyoung/artsshow/MainActivity.java
@@ -0,0 +1,83 @@
+package app.com.example.heeyoung.artsshow;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+
+    public static class PlaceholderFragment extends Fragment {
+
+
+        private ListView m_artsList;
+        private customAdapter m_Adapter;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+            m_Adapter = new customAdapter();
+
+            m_artsList = (ListView) rootView.findViewById(R.id.artslist);
+
+            m_artsList.setAdapter(m_Adapter);
+
+            m_Adapter.add("전희영");
+            m_Adapter.add("구준호");
+
+
+            return rootView;
+        }
+    }
+}
