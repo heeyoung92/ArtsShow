516180456143000f39ed8991cf1f53cfec3ea827
micah <micah@micahui-MacBook-Air.local>
2015-01-22 19:44:35 +0900

    slide 추가 액션바 이상있는 버전

--- app/src/main/java/app/com/example/heeyoung/artsshow/MainActivity.java
+++ app/src/main/java/app/com/example/heeyoung/artsshow/MainActivity.java
@@ -1,11 +1,20 @@
 package app.com.example.heeyoung.artsshow;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,32 +22,72 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.load.engine.cache.DiskLruCacheWrapper;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import app.com.example.heeyoung.artsshow.model.Product;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.tabs)
+    PagerSlidingTabStrip tabs;
+    @InjectView(R.id.pager)
+    ViewPager pager;
+
+    private MyPagerAdapter adapter;
+    private Drawable oldBackground = null;
+    private int currentColor;
+    private SystemBarTintManager mTintManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+//        setSupportActionBar(toolbar);
+        // create our manager instance after the content view is set
+        mTintManager = new SystemBarTintManager(this);
+        // enable status bar tint
+        mTintManager.setStatusBarTintEnabled(true);
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        tabs.setViewPager(pager);
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        pager.setPageMargin(pageMargin);
+        changeColor(getResources().getColor(R.color.green));
+
+        tabs.setOnTabReselectedListener(new PagerSlidingTabStrip.OnTabReselectedListener() {
+            @Override
+            public void onTabReselected(int position) {
+                Toast.makeText(MainActivity.this, "Tab reselected: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /*
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container,new ProductListFragment())
+                    .add(R.id.container, new ProductListFragment())
                     .commit();
         }
+        */
 
         // Glide 캐시 설정 https://github.com/bumptech/glide/wiki/Configuration
         // Disk 100MB, Mem 20MB
@@ -76,6 +125,26 @@ public class MainActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private void changeColor(int newColor)
+    {
+        tabs.setBackgroundColor(newColor);
+        mTintManager.setTintColor(newColor);
+        // change ActionBar color just if an ActionBar is available
+        Drawable colorDrawable = new ColorDrawable(newColor);
+        Drawable bottomDrawable = new ColorDrawable(getResources().getColor(android.R.color.transparent));
+        LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
+        if (oldBackground == null) {
+            getSupportActionBar().setBackgroundDrawable(ld);
+        } else {
+            TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
+            getSupportActionBar().setBackgroundDrawable(td);
+            td.startTransition(200);
+        }
+
+        oldBackground = ld;
+        currentColor = newColor;
+    }
+
     public static class ProductListFragment extends Fragment
     {
         private ArrayAdapter<Product> mProductListAdapter;
@@ -85,12 +154,11 @@ public class MainActivity extends ActionBarActivity
                                  ViewGroup container,
                                  Bundle savedInstanceState)
         {
-            mProductListAdapter = new ProductListAdapter(getActivity(), android.R.layout.simple_list_item_1);
-
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            mProductListAdapter = new ProductListAdapter(getActivity(), android.R.layout.simple_list_item_1);
             ListView productListView = (ListView)rootView.findViewById(R.id.artslist);
             productListView.setAdapter(mProductListAdapter);
-
             updateProducts();
 
             return rootView;
@@ -139,6 +207,32 @@ public class MainActivity extends ActionBarActivity
                     }
                 }
             }
+        }
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = {
+                "최신순", "인기순"
+        };
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ProductListFragment();
         }
     }
 }
