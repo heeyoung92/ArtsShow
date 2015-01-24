f5d4b45d4395b26219893673af418679128cbc48
heeyoung <heeyoung12360@gmail.com>
2015-01-24 14:57:34 +0900

    table 추가

--- app/src/main/java/app/com/example/heeyoung/artsshow/data/ArtsDbHelper.java
+++ app/src/main/java/app/com/example/heeyoung/artsshow/data/ArtsDbHelper.java
@@ -1,10 +1,18 @@
 package app.com.example.heeyoung.artsshow.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import app.com.example.heeyoung.artsshow.data.ArtsContract.BrandEntry;
+import app.com.example.heeyoung.artsshow.data.ArtsContract.ImageEntry;
+import app.com.example.heeyoung.artsshow.data.ArtsContract.ProductEntry;
+
 /**
  * Created by heeyoung on 2015-01-24.
  */
 
-/*
+
 public class ArtsDbHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
@@ -18,7 +26,29 @@ public class ArtsDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        final String SQL_CREATE_BRAND_TABLE = "CREATE TABLE" + BrandEntry.TABLE_NAME
+        final String SQL_CREATE_BRAND_TABLE = "CREATE TABLE" + BrandEntry.TABLE_NAME + "(" +
+                BrandEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                BrandEntry.COLUMN_BRAND_KEY + " INTEGER NOT NULL, " +
+                BrandEntry.COLUMN_BRAND_TITLE + "TEXT NOT NULL, " +
+                BrandEntry.COLUMN_BRAND_COUNTRY + "TEXT NOT NULL);";
+
+        final String SQL_CREATE_PRODUCT_TABLE = "CREATE TABLE" + ProductEntry.TABLE_NAME + "(" +
+                ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                ProductEntry.COLUMN_PRD_KEY + " INTEGER NOT NULL, " +
+                ProductEntry.COLUMN_PRD_TITLE + "TEXT NOT NULL, " +
+                ProductEntry.COLUMN_PRD_DESC + "TEXT NOT NULL);";
+
+        final String SQL_CREATE_IMAGE_TABLE = "CREATE TABLE" + ImageEntry.TABLE_NAME + "(" +
+                ImageEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                ImageEntry.COLUMN_IMAGE_KEY + " INTEGER NOT NULL, " +
+                ImageEntry.COLUMN_IMAGE_URL + "TEXT NOT NULL, " +
+                ImageEntry.COLUMN_BRAND_KEY + "TEXT NOT NULL);";
+
+//        sqLiteDatabase.execSQL(SQL_CREATE_BRAND_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 }
-*/
